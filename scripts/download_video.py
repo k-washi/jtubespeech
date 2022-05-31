87ebbb7eafacc6b7bb1224e507a11ac12b3968d2
k-washi <bandad.kw@gmail.com>
2022-05-31 08:10:23 +0000

    data download start

--- scripts/download_video.py
+++ scripts/download_video.py
@@ -8,6 +8,9 @@ from pathlib import Path
 from util import make_video_url, make_basename, vtt2txt, autovtt2txt
 import pandas as pd
 from tqdm import tqdm
+import os
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
 def parse_args():
   parser = argparse.ArgumentParser(
@@ -20,19 +23,7 @@ def parse_args():
   parser.add_argument("--keeporg",    action='store_true', default=False, help="keep original audio file.")
   return parser.parse_args(sys.argv[1:])
 
-def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False):
-  """
-  Tips:
-    If you want to download automatic subtitles instead of manual subtitles, please change as follows.
-      1. replace "sub[sub["sub"]==True]" of for-loop with "sub[sub["auto"]==True]"
-      2. replace "--write-sub" option of youtube-dl with "--write-auto-sub"
-      3. replace vtt2txt() with autovtt2txt()
-      4 (optional). change fn["vtt"] (path to save subtitle) to another. 
-  """
-
-  sub = pd.read_csv(fn_sub)
-
-  for videoid in tqdm(sub[sub["sub"]==True]["videoid"]): # manual subtitle only
+def download_video_job(videoid, lang, outdir, wait_sec, keep_org):
     fn = {}
     for k in ["wav", "wav16k", "vtt", "txt"]:
       fn[k] = Path(outdir) / lang / k / (make_basename(videoid) + "." + k[:3])
@@ -44,15 +35,15 @@ def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False):
       # download
       url = make_video_url(videoid)
       base = fn["wav"].parent.joinpath(fn["wav"].stem)
-      cp = subprocess.run(f"youtube-dl --sub-lang {lang} --extract-audio --audio-format wav --write-sub {url} -o {base}.\%\(ext\)s", shell=True,universal_newlines=True)
+      cp = subprocess.run(f"youtube-dl --sub-lang {lang} --write-sub {url} --extract-audio --audio-format wav -o {base}.\%\(ext\)s", shell=True,universal_newlines=True)
       if cp.returncode != 0:
         print(f"Failed to download the video: url = {url}")
-        continue
+        return
       try:
         shutil.move(f"{base}.{lang}.vtt", fn["vtt"])
       except Exception as e:
         print(f"Failed to rename subtitle file. The download may have failed: url = {url}, filename = {base}.{lang}.vtt, error = {e}")
-        continue
+        return
 
       # vtt -> txt (reformatting)
       try:
@@ -61,7 +52,7 @@ def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False):
           f.writelines([f"{t[0]:1.3f}\t{t[1]:1.3f}\t\"{t[2]}\"\n" for t in txt])
       except Exception as e:
         print(f"Falied to convert subtitle file to txt file: url = {url}, filename = {fn['vtt']}, error = {e}")
-        continue
+        return
 
       # wav -> wav16k (resampling to 16kHz, 1ch)
       try:
@@ -70,7 +61,7 @@ def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False):
         wav.export(fn["wav16k"], format="wav", bitrate="16k")
       except Exception as e:
         print(f"Failed to normalize or resample downloaded audio: url = {url}, filename = {fn['wav']}, error = {e}")
-        continue
+        return
 
       # remove original wav
       if not keep_org:
@@ -79,6 +70,25 @@ def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False):
       # wait
       if wait_sec > 0.01:
         time.sleep(wait_sec)
+      
+
+def download_video(lang, fn_sub, outdir="video", wait_sec=3, keep_org=False, max_worker=8):
+  """
+  Tips:
+    If you want to download automatic subtitles instead of manual subtitles, please change as follows.
+      1. replace "sub[sub["sub"]==True]" of for-loop with "sub[sub["auto"]==True]"
+      2. replace "--write-sub" option of youtube-dl with "--write-auto-sub"
+      3. replace vtt2txt() with autovtt2txt()
+      4 (optional). change fn["vtt"] (path to save subtitle) to another. 
+  """
+  print(lang)
+  print(fn_sub)
+  print(outdir)
+  sub = pd.read_csv(fn_sub)
+  print("DATA LEN: ", len(sub))
+  with ThreadPoolExecutor(max_workers=max_worker,  thread_name_prefix="thread") as executor:
+    fs = [executor.submit(download_video_job, videoid, lang, outdir, wait_sec, keep_org) for videoid in tqdm(sub[sub["sub"]==True]["videoid"].values.tolist())]
+  print("End")
 
   return Path(outdir) / lang
 
